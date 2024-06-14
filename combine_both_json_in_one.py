5e9467f68fd48e3e4fee8bcce797c59a4021a6a8
Dipyomoy <ayan.barua25@gmail.com>
2024-06-14 13:47:23 +0530

    minor change

--- combine_both_json_in_one.py
+++ combine_both_json_in_one.py
@@ -1,5 +1,6 @@
 fruits = {'apple': 2, 'banana': 3, 'orange': 1}
 card = {'egg': 1, 'watermelon': 2, 'coconut': 1}
+
 combined = card.copy()
 for key, value in fruits.items():
     if key not in combined:
